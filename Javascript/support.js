5d9d11b176505967061b08e965da2629ceffef81
akuttiyan <akuttiyan@eastsideprep.org>
2024-01-16 13:15:27 -0800

    added text to suppot page

--- Javascript/support.js
+++ Javascript/support.js
@@ -1,3 +1,8 @@
 function changepagetoHomeScreen() {
   window.location.href = "../index.html";
 }
+
+function changePageToActivities() {
+  window.location.href = "../HTML/Courses and Activities/Activity1.html";
+
+}
