939d25459593739d60969ee85531ad5880e005e2
user894095 <153703236+user894095@users.noreply.github.com>
2024-03-17 21:58:44 -0700

    changes

--- Javascript/cyberminds.js
+++ Javascript/cyberminds.js
@@ -148,6 +148,8 @@ buttonsInWholething.forEach((button) => {
 const sections = document.querySelectorAll(
   ".section2, .section5, .section4, .section3"
 );
+
+// changes
 const cursors = document.querySelectorAll(".cursor");
 
 // Initialize all cursors as inactive
